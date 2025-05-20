cf115fc906e73078b8daf3628c4f30dbb78f2aa1
vijayabuiders <It.vijayabuilder@gmail.com>
2025-05-20 13:45:39 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
-import AdminPage from "./pages/AdminPage.tsx"; // Adjust path and name as needed
-import './index.css';
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/admin" element={<AdminPage />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
